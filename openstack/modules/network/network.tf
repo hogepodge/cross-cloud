8e69d81f2825bf1bb30141e1580d186ba85ce21c
Chris Hoge <chris@openstack.org>
2018-03-03 04:03:45 +0000

    Refactor WIP

--- openstack/modules/network/network.tf
+++ openstack/modules/network/network.tf
@@ -1,21 +1,59 @@
-resource "openstack_networking_network_v2" "cncf" {
+### Internal Network
+resource "openstack_networking_network_v2" "k8s" {
   name           = "k8s-internal"
   admin_state_up = "true"
 }
 
-resource "openstack_networking_subnet_v2" "cncf" {
-  network_id = "${ openstack_networking_network_v2.cncf.id }"
+resource "openstack_networking_subnet_v2" "internal" {
+  network_id = "${ openstack_networking_network_v2.internal.id }"
   cidr = "${ var.internal_network_cidr }"
   dns_nameservers  = [ "8.8.8.8" ]
 }
 
-resource "openstack_networking_router_v2" "cncf" {
+resource "openstack_networking_router_v2" "internal" {
   name = "router-k8s-internal"
   external_gateway = "${ var.external_network_id }"
 }
 
-resource "openstack_networking_router_interface_v2" "cncf" {
-  router_id = "${ openstack_networking_router_v2.cncf.id }"
-  subnet_id = "${ openstack_networking_subnet_v2.cncf.id }"
+resource "openstack_networking_router_interface_v2" "internal" {
+  router_id = "${ openstack_networking_router_v2.internal.id }"
+  subnet_id = "${ openstack_networking_subnet_v2.internal.id }"
+}
+
+#### Security Groups
+
+resource "openstack_networking_secgroup_v2" "k8s" {
+  name        = "k8s"
+  description = "Cross Cloud Security Group (ssh, http, https)"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "k8s_ssh" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 22
+  port_range_max    = 22
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${ openstack_networking_secgroup_v2.k8s.id }"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "k8s_etcd" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = "2379" 
+  port_range_max    = "2381"
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${ openstack_networking_secgroup_v2.k8s.id }"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "k8s_https" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = "${ openstack_networking_secgroup_v2.k8s.id }"
 }
 
